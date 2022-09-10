d45558f2e33e9da0325b56fb50c35708415877c8
Sergey Korshikov <seregakor@mail.ru>
2022-09-11 00:13:11 +0300

    dev learn app pizza

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,13 +1,18 @@
+import React from "react";
+
 function Categories() {
+  const [categoryIndex, setCategoryIndex] = React.useState(0);
+
+  const categoriesList = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые'];
+
   return (
     <div className='categories'>
       <ul>
-        <li className='active'>Все</li>
-        <li>Мясные</li>
-        <li>Вегетарианская</li>
-        <li>Гриль</li>
-        <li>Острые</li>
-        <li>Закрытые</li>
+        {categoriesList.map((item, index) => (
+          <li key={index} className={index === categoryIndex ? 'active' : ''} onClick={() => setCategoryIndex(index)}>
+            {item}
+          </li>
+        ))}
       </ul>
     </div>
   );
