eb2dca6d625e965b3e56de4e1b02592caa78a8ba
Sergey Korshikov <seregakor@mail.ru>
2022-09-10 15:12:50 +0300

    dev app pizza-learn

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import Categories from './components/Categories';
+import Header from './components/Header'
+import PizzaBlock from './components/PizzaBlock';
+import Sort from './components/Sort';
+import './scss/app.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='wrapper'>
+        <Header />
+        <div className='content'>
+          <div className='container'>
+            <div className='content__top'>
+              <Categories />
+              <Sort />
+            </div>
+            <h2 className='content__title'>Все пиццы</h2>
+            <div className='content__items'>
+              <PizzaBlock title="Мексиканская" price={500} />
+              <PizzaBlock title="Мексиканская 1" price={1500} />
+              <PizzaBlock title="Мексиканская" price={500} />
+              <PizzaBlock title="Мексиканская" price={500} />
+              <PizzaBlock title="Мексиканская" price={500} />
+              <PizzaBlock title="Мексиканская" price={500} />
+              <PizzaBlock title="Мексиканская" price={500} />
+              <PizzaBlock title="Мексиканская" price={500} />
+              <PizzaBlock title="Мексиканская" price={500} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
